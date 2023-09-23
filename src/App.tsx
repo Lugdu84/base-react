4f3b35652cf589a18235d5c4e8a73e73d9316a66
David Grammatico <david.lugdunum@gmail.com>
2023-09-23 17:12:35 +0200

    add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Form } from "./components/Form"
 import { Title } from "./components/Title"
 
 function App() {
 
   return <>
     <Title name="Apprendre React avec Vite" color="blue" />
+    <Form />
   </>
 }
 
