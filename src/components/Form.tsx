d9bbb59345d2769b9b52a14ee026745f770bc668
David Grammatico <david.lugdunum@gmail.com>
2023-09-23 17:14:34 +0200

    add inputs and button submit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,6 +4,8 @@ export const Form = () => {
   return (
       <form>
           <input type="text" />
+          <input type="checkbox" />
+          <button type="submit">Envoyer</button>
     </form>
   )
 }
