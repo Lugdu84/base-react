e72b7b29d614954af36d6372acb60d265ea453af
David Grammatico <david.lugdunum@gmail.com>
2023-09-30 12:59:54 +0200

    add useMemo for passwordSecurity

--- src/components/FormControllled.tsx
+++ src/components/FormControllled.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import { CGUCheckbox } from "./CGUCheckbox"
 import Input from "./Input"
 
+
+const passwordSecurity = (test: string) => test.length > 8 ? 'Fort' : 'Faible'
 
 export const FormControlled = () => {
   const [mail, setMail] = useState('')
@@ -25,16 +27,20 @@ export const FormControlled = () => {
     event.preventDefault()
   }
 
-  const passwordSecurity = password.length > 8 ? 'Fort' : 'Faible'
+  const security = useMemo(() => {
+    console.log('Calcul de la sécurité')
+    return passwordSecurity(password)
+  }, [password])
+
   
   return (
-    <form className=" w-96 flex flex-col gap-2" onSubmit={handleSubmit}>
-      <h2>Form Controlled</h2>
+    <form className=" w-9/12 md:w-1/2 flex flex-col gap-2 p-2 border border-blue-700 rounded-lg" onSubmit={handleSubmit}>
+      <h2 className=" text-center">Form Controlled</h2>
       <Input name="mail" type="text" onChange={handleChangeMail} value={mail} label="Mail" />
       <Input name="password" type="password" onChange={handleChangePassword} value={password} label="Mot de passe" />
-      <p>La sécurité de votre mot de passe est : {passwordSecurity}</p>
+      <p>La sécurité de votre mot de passe est : {security}</p>
       <CGUCheckbox checked={checked} onCheck={toggleChecked} />
-      <button disabled={!checked} type="submit">Envoyer</button>
+      <button className="w-full bg-green-600 rounded-md py-2" disabled={!checked} type="submit">Envoyer</button>
     </form>
   )
 }
