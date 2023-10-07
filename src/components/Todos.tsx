38937270b4efc3c65a527e8ed4836856753d4c43
David Grammatico <david.lugdunum@gmail.com>
2023-10-07 22:53:14 +0200

    update reducer with showCompleted action

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -18,8 +18,13 @@ type Action = {
 } | {
     type: 'REMOVE_CHECKED_TODOS'
 }
+    |
+{
+    type: 'SHOW_COMPLETED'
+}
 type State = {
     todos: Todo[]
+    showCompleted: boolean
 }
 
 export default function Todos() {
@@ -34,6 +39,9 @@ export default function Todos() {
                 return { ...state, todos: state.todos.map((todo: Todo) => todo.id === action.payload.id ? { ...todo, completed: !todo.completed } : todo) }
             case 'REMOVE_CHECKED_TODOS':
                 return { ...state, todos: state.todos.filter((todo: Todo) => !todo.completed) }
+            
+            case 'SHOW_COMPLETED':
+                return { ...state, showCompleted: !state.showCompleted }
             default:
                 return state
         }
@@ -44,22 +52,31 @@ export default function Todos() {
             { id: 2, title: 'Faire la lessive', completed: false },
             { id: 3, title: 'Faire le ménage', completed: false },
         ],
+        showCompleted: false
     })
 
+    const visibleTodos = state.showCompleted ? state.todos : state.todos.filter((todo: Todo) => !todo.completed)
+
     return (
-      <div className='grid grid-cols-1 gap-4'>
+        <div className='grid grid-cols-1 gap-4'>
+            <div className='flex gap-2'>
+                <input name='hideCompleted' type="checkbox" checked={state.showCompleted} onChange={() => dispatch({type: 'SHOW_COMPLETED'})} />
+                <label htmlFor="hideCompleted">Afficher les tâches accomplies</label>
+            </div>
+            
+                
         <ul className='flex flex-col gap-4'>
-            {state.todos.map((todo) =>
+            {visibleTodos.map((todo) =>
                 <li className='flex gap-2 items-center justify-between' key={todo.id}>
                     <input type="checkbox" checked={todo.completed} onChange={() => dispatch({type: 'TOGGLE_TODO', payload: todo})} />
                     {todo.title}
                     <button
                         onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}
-                        className='bg-red-500 py-2 px-4 rounded-md'
+                        className='bg-red-400 py-2 px-4 rounded-md'
                     >Supprimer</button>
                 </li>)}
             </ul>
-            <button className='bg-red-500 py-2 px-4 rounded-md' onClick={() => dispatch({type: 'REMOVE_CHECKED_TODOS'})}>Supprimer les tâches accomplies</button>
+            <button className='bg-red-400 py-2 px-4 rounded-md' onClick={() => dispatch({type: 'REMOVE_CHECKED_TODOS'})}>Supprimer les tâches accomplies</button>
       </div>
   )
 }
